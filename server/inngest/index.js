c4f532cf014ba076c9745681942291b7bf39e197
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-08-12 19:41:35 +0530

    email remainder added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 export const inngest = new Inngest({ id: "pingup-app" });
 
@@ -65,9 +66,69 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+//innngest function to send reminder when a new connection request is added
+
+const sendNewConnectionRequestRemainder=inngest.createFunction(
+    {id:'send-new-connection-request-reminder'},
+    {event:"app/connection-request"},
+    async({event,step})=>{
+        const {connectionId}=event.data;
+
+        await step.run('send-connection-request-mail',async()=>{
+            const connection=await Connection.findById(connectionId).populate('from_user_id to_user_id');
+
+            const subject=`👋 New Connection Request`;
+
+            const body=`
+            <div style="font-family:Arial,sans-serif; padding:20px;">
+            <h2>Hi ${connection.to_user_id.full_name},</h2>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#l0b981;">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks ,<br/>PingUp- Stay Connected</p>
+            </div>`
+
+            await sendEmail({
+                to:connection.to_user_id.email,
+                subject,
+                body
+            })
+        })
+
+        const in24Hours=new Date(Date.now()+24*60*60*1000)
+        await step.sleepUntil('wait-for-24-hours',in24Hours);
+        await step.run('send-connection-request-reminder',async()=>{
+            const connection=await Connection.findById(connectionId).populate('from_user_id to_user_id')
+
+            if(connection.accepted==='accepted'){
+                return {message:'Already accepted'}
+            }
+
+            const subject=`👋 New Connection Request`;
+
+            const body=`
+            <div style="font-family:Arial,sans-serif; padding:20px;">
+            <h2>Hi ${connection.to_user_id.full_name},</h2>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#l0b981;">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks ,<br/>PingUp- Stay Connected</p>
+            </div>`
+
+            await sendEmail({
+                to:connection.to_user_id.email,
+                subject,
+                body
+            })
+            return {message:'Remainder sent'}
+        })
+    }
+)
+
 export const functions = [
   logEvent, // Keep this for debugging
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
+  sendNewConnectionRequestRemainder
 ];
--- server/server.js
+++ server/server.js
@@ -4,17 +4,20 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
-
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from "./routes/userRoutes.js";
 const app = express();
 
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("🚀 Server is running"));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+app.use('/api/user',userRouter)
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
